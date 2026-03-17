69d2aab1f19575523a9910c6b5f617f0035d5f15
Mubashshir Qamar <qamarmubashshir3@gmail.com>
2026-03-17 13:42:42 +0530

    Update helloworld_demo.java

--- helloworld_demo.java
+++ helloworld_demo.java
@@ -1,7 +1,11 @@
-pubic class HelloApp
-{
-public static void main("String[]args)
-{
-System.out.println("Hello,World!");
-}
+public class HelloApp {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+        String result = String.join(", ", args);
+        System.out.println("Hello, " + result + "!");
+    }
 }
