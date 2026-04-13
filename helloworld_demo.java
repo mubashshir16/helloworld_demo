7384e6940de8fa395035754c09b037692ba3cc4a
Mubashshir Qamar <qamarmubashshir3@gmail.com>
2026-04-13 13:51:23 +0530

    Update helloworld_demo.java

--- helloworld_demo.java
+++ helloworld_demo.java
@@ -1,16 +1,13 @@
 public class HelloApp {
     public static void main(String[] args) {
+
+        if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            StringBuilder greeting = new StringBuilder("Hello, ");
 
-            for (String name : args) {
-                greeting.append(name).append(", ");
-            }
+            String names = String.join(", ", args);
 
-            String finalGreeting = greeting.substring(0, greeting.length() - 2);
-
-            System.out.println(finalGreeting + "!");
+            System.out.println("Hello, " + names + "!");
         }
     }
 }
